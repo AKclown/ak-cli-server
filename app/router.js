54e7305c76cee453cadc49132456e1dc3f80c46b
AKclown <13413629530@139.com>
2022-06-01 08:02:43 +0800

    完成egg-socket.io的接入

--- app/router.js
+++ app/router.js
@@ -6,4 +6,12 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/project/template', controller.project.getTemplate);
+
+  // app.io.of('/')
+  app.io.route('chat', app.io.controller.chat.index);
+
+  // app.io.of('/chat')
+  // 这个of是什么含义呢？ app.io讲的就是我们的websocket服务，那么这个of就是将我们的路由namespace和我们的middleware进行映射。
+  // 每一个websocket首先会发起一个http请求来获取这个链接。所以/chat完成的就是第一次http链接，而链接之后在进行的服务之间的映射通过后面的route来处理
+  app.io.of('/chat').route('chat', app.io.controller.chat.index);
 };
--- config/config.default.js
+++ config/config.default.js
@@ -18,6 +18,20 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  // 跟demo有点差异，因为demo直接导出这个属性
+  config.io = {
+    namespace: {
+      '/': {
+        connectionMiddleware: ['auth'],
+        packetMiddleware: ['filter'],
+      },
+      '/chat': {
+        connectionMiddleware: ['auth'],
+        packetMiddleware: [],
+      },
+    },
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
